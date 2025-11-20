f9fde38a23048c0aadeb442353c1c387c94915bf
Jessica Kiroff <jesskiroff@gmail.com>
2025-11-19 20:51:47 -0500

    add question object

---
+++ question_model.py
@@ -0,0 +1,10 @@
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+question_1 = Question("The question model works.", "True")
+question_2 = Question("2 + 2 equals 5.", "False")
+
+print(question_1.text)
